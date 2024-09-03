17e9295551afac1c470fab7c9874a5a1df3fe2e8
sajad shokraei <shockzbeatbox@gmail.com>
2024-09-03 20:56:11 +0330

    update

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+
 const Analytics = () => {
   return (
     <div className="flex justify-center items-center h-[80%]">
       <p className="font-semibold text-5xl text-blue-600 animate-pulse">Comming soon . . .</p>
+     
     </div>
   )
 }
